db4d5937f34d7b4c6664837cd3ff1aa7bf093398
Lenjoy <picasa.lenjoy@gmail.com>
2018-09-16 09:53:35 -0700

    Add HelloService.getRelevance in thrift

--- go/src/simple_thrift_service/thrift_def/hello.thrift
+++ go/src/simple_thrift_service/thrift_def/hello.thrift
@@ -8,12 +8,25 @@ namespace go simple_thrift_service.thrift_gen.hello
 
 struct HelloRequest {
     1: optional string message;
+    2: optional i32 input_id;
+    3: optional i16 dimension;
 }
 
 struct HelloResponse {
     1: optional string message;
+    2: optional list<HelloDoc> results;
+}
+
+// doc info
+struct HelloDoc {
+	1: i32 doc_id;
+	2: list<double> vec;
+	3: optional double score;
 }
 
 service HelloService {
+    // Simple message process and response
     HelloResponse sendMessage(1: HelloRequest request);
+    // Get relevant content by given input_id
+    HelloResponse getRelevance(1: HelloRequest request);
 }
